5fadcea03ac9cb3b56ada64b37acce79204b74bb
Paul Highum <phigh07@gmail.com>
2018-02-26 09:30:57 -0700

    adds migration schema

--- migrations/20180226092620_quotes.js
+++ migrations/20180226092620_quotes.js
@@ -1,8 +1,11 @@
+exports.up = function(knex, Promise){
+  return knex.schema.createTable("quotes", table => {
+    table.increments("id").primary()
+    table.text("author")
+    table.text("quote")
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise){
+  return knex.schema.dropTableIfExists("quotes")
+}
--- migrations/20180226092627_comments.js
+++ migrations/20180226092627_comments.js
@@ -1,8 +1,11 @@
+exports.up = function(knex, Promise){
+  return knex.schema.createTable("comments", table => {
+    table.increments("id").primary()
+    table.integer("quote_id").references("quotes.id")
+    table.text("comment")
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise){
+  return knex.schema.deleteTableIfExists("comments")
+}
